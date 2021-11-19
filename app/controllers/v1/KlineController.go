74ac6f861edf0c270d13922b3a68f328e5b886ad
ye zhiming <mail@54zm.com>
2021-11-20 00:38:16 +0800

    先下拉=>后上传==================更新代码

--- app/controllers/v1/KlineController.go
+++ app/controllers/v1/KlineController.go
@@ -7,6 +7,7 @@ import (
 	"goapi/pkg/logger"
 	"goapi/pkg/websocket"
 	"strings"
+	"time"
 )
 
 // k线图服务
@@ -15,31 +16,31 @@ type KlineController struct {
 	BaseController
 }
 
-// WsHandler socket
+// WsHandler socket  优化关闭socket链接
 func (h *KlineController) WsHandler(c *gin.Context) {
 	ws, err := wss.GetSocket(c)
 	if err != nil {
 		logger.Error(err)
 		return
 	}
+	market, NewMarketErr := huobiapi.NewMarket()
+	if NewMarketErr != nil {
+		logger.Error(NewMarketErr)
+		return
+	}
 	for {
-		market, NewMarketErr := huobiapi.NewMarket()
-		if NewMarketErr != nil {
-			logger.Error(NewMarketErr)
-			continue
-		}
 		//读取ws中的数据
 		mt, message, ReadMessageErr := ws.Conn.ReadMessage()
 		if ReadMessageErr != nil {
-			logger.Error(ReadMessageErr)
-			marketErr := market.Close()
-			if marketErr != nil {
-				logger.Error(marketErr)
-				continue
-			} else {
+			marketErr := ws.Close()
+			if marketErr == nil {
 				logger.Info("关闭成功")
+			} else {
+				logger.Error(marketErr)
+				logger.Error(ReadMessageErr)
+				return
 			}
-			continue
+			return
 		}
 		logger.Info(fmt.Sprintf("当前数据类型为: %v", mt))
 		//对数据进行切割，读取参数
@@ -50,42 +51,9 @@ func (h *KlineController) WsHandler(c *gin.Context) {
 		logger.Info(newMsg)
 		if strings.Contains(msg, "1min") || strings.Contains(msg, "step1") {
 			logger.Info("当前为1分钟请求")
-			SubscribeErr := market.Subscribe(newMsg, func(topic string, json *huobiapi.JSON) {
-				logger.Info(msg)
-				logger.Info(newMsg)
-				if err != nil {
-					logger.Error(err)
-				}
-				logger.Info("订阅成功")
-				// 收到数据更新时回调
-				logger.Info(topic)
-				logger.Info(json)
-				// 火币网推送的回来的数据转换为字符串
-				msgData, MarshalJSONErr := json.MarshalJSON()
-				if MarshalJSONErr != nil {
-					logger.Error(MarshalJSONErr)
-					return
-				}
-				ws.Mux.Lock()
-				WriteMessageErr := ws.Conn.WriteMessage(mt, msgData)
-				ws.Mux.Unlock()
-				if WriteMessageErr != nil {
-					logger.Error(WriteMessageErr)
-					wsErr := ws.Close()
-					if wsErr != nil {
-						logger.Error(wsErr)
-					}
-					return
-				}
-				//返回数据给用户
-			})
-			// socket 火币网失败，释放资源
-			if SubscribeErr != nil {
-				CloseErr := market.Close()
-				if CloseErr != nil {
-					logger.Error(CloseErr)
-					continue
-				}
+			for i := 1; i <= 5; i++ { // 客户端没socket一次，我们就像火币网发出五次
+				go SocketHuoBi(ws, market, NewMarketErr, mt, newMsg)
+				time.Sleep(2 * time.Second)
 			}
 		} else {
 			//写入ws数据
@@ -166,5 +134,57 @@ func (h *KlineController) WsHandler(c *gin.Context) {
 			//}()
 		}
 
+	}
+}
+
+func SocketHuoBi(ws *wss.WsConn, market *huobiapi.Market, NewMarketErr error, mt int, parentTopic string) {
+	if NewMarketErr != nil {
+		err := market.Close()
+		if err != nil {
+			return
+		}
+	}
+	//if closeRoutine.(int) > 0 {
+	//	logger.Info(fmt.Sprintf("当前 goroutine 是否需要关闭：%v", closeRoutine))
+	//	marketErr := ws.Close()
+	//	if marketErr == nil {
+	//		logger.Info("goroutine 关闭成功")
+	//	} else {
+	//		logger.Error(marketErr)
+	//		return
+	//	}
+	//}
+	SubscribeErr := market.Subscribe(parentTopic, func(topic string, json *huobiapi.JSON) {
+		logger.Info("成功订阅火币网")
+		logger.Info(fmt.Sprintf("您当前订阅的 topic 为：%v", parentTopic))
+		// 收到数据更新时回调
+		logger.Info(topic)
+		logger.Info(json)
+		// 火币网推送的回来的数据转换为字符串
+		msgData, MarshalJSONErr := json.MarshalJSON()
+		if MarshalJSONErr != nil {
+			logger.Error(MarshalJSONErr)
+			return
+		}
+		ws.Mux.Lock()
+		WriteMessageErr := ws.Conn.WriteMessage(mt, msgData)
+		ws.Mux.Unlock()
+		if WriteMessageErr != nil {
+			logger.Error(WriteMessageErr)
+			wsErr := ws.Close()
+			if wsErr != nil {
+				logger.Error(wsErr)
+			}
+			return
+		}
+		//返回数据给用户
+	})
+	// socket 火币网失败，释放资源
+	if SubscribeErr != nil {
+		CloseErr := market.Close()
+		if CloseErr != nil {
+			logger.Error(CloseErr)
+			return
+		}
 	}
 }
