e1906d08623cf5af26b3fcff604983633a72fa2a
ye zhiming <mail@54zm.com>
2021-12-09 19:16:57 +0800

    测试demo

--- test/main.go
+++ test/main.go
@@ -1,38 +1,49 @@
 package main
 
 import (
-	"encoding/base64"
 	"fmt"
-	"goapi/app/models"
-	"goapi/app/response"
-	"goapi/bootstrap"
 	"goapi/config"
+	"goapi/pkg/helpers"
 	"goapi/pkg/logger"
-	"goapi/pkg/mysql"
 	"io/ioutil"
+	"net/http"
+	"time"
 )
 
 func init() {
 	// 初始化配置信息
 	config.Initialize()
+	// 定义日志目录
+	logger.Init("test")
 }
 
 func main() {
-	// 初始化 SQL
-	fmt.Println("初始化 SQL")
-	bootstrap.SetupDB()
-	var user response.User
-	db := mysql.DB
-	//db.Debug().Model(models.User{}).Where("id", "503").Find(&user)
-	var v1 models.Verify
-	db.Debug().Model(models.Verify{}).Where("user_id", "501").Find(&v1)
-	imgbase64 := v1.ImgCardFront
-	imgbff, _ := base64.StdEncoding.DecodeString(imgbase64)
-	fmt.Println(len(imgbff))
-	err2 := ioutil.WriteFile("./test.jpg", imgbff, 0666)
-	if err2 != nil {
-		logger.Error(err2)
-		return
+	start := true
+	for {
+		if start == true {
+			for i := 1; i <= 4000; i++ {
+				url := "http://10.10.10.80:5000/?page=" + helpers.IntToString(i)
+				go func(url string, i int) {
+					logger.Info(fmt.Sprintf("执行地%v条数据", i))
+					get, err := http.Get(url)
+					if err != nil {
+						logger.Error(err)
+					}
+					_, err1 := ioutil.ReadAll(get.Body)
+					if err1 != nil {
+						logger.Error(err1)
+					}
+					//logger.Info(string(Body))
+				}(url, i)
+				if i == 4000 {
+					logger.Info("线程任务分配完毕")
+				}
+				// 每十页数据休息一秒
+				if (i % 2) == 0 {
+					time.Sleep(time.Second * 1)
+				}
+			}
+			start = false
+		}
 	}
-	fmt.Println(user)
 }
